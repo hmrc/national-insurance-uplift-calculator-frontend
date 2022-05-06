3bbc7700b47e9c83d44a14d185ac6a8b61162b6e
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-05-06 12:20:40 +0100

    Add currency formatter / mapping / constraints

--- app/forms/mappings/Constraints.scala
+++ app/forms/mappings/Constraints.scala
@@ -16,8 +16,9 @@
 
 package forms.mappings
 
-import java.time.LocalDate
+import config.CurrencyFormatter
 
+import java.time.LocalDate
 import play.api.data.validation.{Constraint, Invalid, Valid}
 
 trait Constraints {
@@ -108,5 +109,25 @@ trait Constraints {
         Valid
       case _ =>
         Invalid(errorKey)
+    }
+
+  protected def minimumCurrency(minimum: BigDecimal, errorKey: String)(implicit ev: Ordering[BigDecimal]): Constraint[BigDecimal] =
+    Constraint {
+      input =>
+        if (input >= minimum) {
+          Valid
+        } else {
+          Invalid(errorKey, CurrencyFormatter.currencyFormat(minimum))
+        }
+    }
+
+  protected def maximumCurrency(maximum: BigDecimal, errorKey: String)(implicit ev: Ordering[BigDecimal]): Constraint[BigDecimal] =
+    Constraint {
+      input =>
+        if (input <= maximum) {
+          Valid
+        } else {
+          Invalid(errorKey, CurrencyFormatter.currencyFormat(maximum))
+        }
     }
 }
