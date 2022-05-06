da3f58559ad08f850cd8f27c22937e9219a152d6
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-05-06 14:03:05 +0100

    Navigate from Salary to Result

--- app/navigation/Navigator.scala
+++ app/navigation/Navigator.scala
@@ -27,6 +27,7 @@ import models._
 class Navigator @Inject()() {
 
   private val normalRoutes: Page => UserAnswers => Call = {
+    case SalaryPage => _ => routes.ResultController.onPageLoad()
     case _ => _ => routes.IndexController.onPageLoad
   }
 
