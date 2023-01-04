249c1d0b6eea39dd7be73974c695ea117c6d0f14
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2023-01-04 16:05:26 +0000

    DL-9362 fixing PR comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,6 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "4.1.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.11.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "5.25.0",
-    "uk.gov.hmrc"       %% "play-language"                  % "5.3.0-play-28",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.68.0"
   )
 
