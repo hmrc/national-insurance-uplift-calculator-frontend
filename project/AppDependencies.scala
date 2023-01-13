b4ae7eb58c818f0d87fb9961e21e53d27e3a1018
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2023-01-13 13:01:54 +0000

    DL-9506 update play frontend hmrc to latest plus other dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,22 +5,22 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "4.1.0-play-28",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.11.0-play-28",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "6.2.0-play-28",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.12.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "5.25.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.68.0"
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.74.0"
   )
 
   val test = Seq(
-    "org.scalatest"           %% "scalatest"               % "3.2.12",
+    "org.scalatest"           %% "scalatest"               % "3.2.15",
     "org.scalatestplus"       %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus"       %% "mockito-3-4"             % "3.2.10.0",
     "org.scalatestplus.play"  %% "scalatestplus-play"      % "5.1.0",
-    "org.jsoup"               %  "jsoup"                   % "1.15.2",
+    "org.jsoup"               %  "jsoup"                   % "1.15.3",
     "com.typesafe.play"       %% "play-test"               % PlayVersion.current,
-    "org.mockito"             %% "mockito-scala"           % "1.17.7",
-    "org.scalacheck"          %% "scalacheck"              % "1.16.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28" % "0.68.0",
+    "org.mockito"             %% "mockito-scala"           % "1.17.12",
+    "org.scalacheck"          %% "scalacheck"              % "1.17.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28" % "0.74.0",
     "com.vladsch.flexmark"    %  "flexmark-all"            % "0.62.2"
   ).map(_ % "test, it")
 
